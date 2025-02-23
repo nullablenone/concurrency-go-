8a97672a6d042b1588f6eff8d879c563bcdb0bbe
useryesa9@gmail.com <useryesa9@gmail.com>
2025-02-23 14:37:55 +0700

    buffred

--- channel.go
+++ channel.go
@@ -5,12 +5,19 @@ import (
 )
 
 func main() {
-	ch := make(chan string)
+	ch := make(chan string, 2)
 
 	go func() {
-		ch <- "menambah data ke channel"
-		fmt.Println("other go routines")
+		ch <- "menambah data ke channel 1"
+	}()
+	go func() {
+		ch <- "menambah data ke channel 2"
+	}()
+	go func() {
+		ch <- "menambah data ke channel 3"
 	}()
 
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 	fmt.Println(<-ch)
 }
